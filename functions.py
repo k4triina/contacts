84963817fae24076b0c558cf29cc8ac1f13ebfbd
k4triina <114050742+k4triina@users.noreply.github.com>
2023-03-01 09:13:41 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,8 @@
+def sareizinat_ar_divi(x):
+    return 2 * x                #definēšana (define)
+print(sareizinat_ar_divi(30))   #izsaukšana (call)
+
+def say_hello_to(name):
+    print(f"Čau, {name}, priecīgus svētkus!")
+
+say_hello_to('Bots')
